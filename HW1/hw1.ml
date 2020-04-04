336476fcb97aa4bcf45741ba5a17ea653c6b156e
henrygenus <henry.a.genus@gmail.com>
2020-04-04 03:23:33 -0700

    Update hw1.ml

--- HW1/hw1.ml
+++ HW1/hw1.ml
@@ -34,21 +34,21 @@ type ('nonterminal, 'terminal) symbol =
   | N of 'nonterminal
   | T of 'terminal
 
+(* given a list of symbols, produce (symbol, tree) grammars for each symbol *)
 let rec make_grammar (nodes: 'N list) tree =
   match (nodes) with
   | [] -> []
   | _ -> List.cons (List.hd nodes, tree) (make_grammar (List.tl nodes) tree)
 
-let map_and_merge_trees f (g : 'N * ('N * ('N, 'T) symbol list) list) tree =
-  let rt, subtree = (f g) in set_union subtree tree
-
-(* try filtering in with function with recursion only *)
-let rec filter_reachable ((rt : 'N ), (tree : ('N * ('N, 'T) symbol list) list))  =
+(*get_subtrees? etc*)
+let rec filter_and_stick_subtree (operand : 'N * ('N * ('N, 'T) symbol list) list) tree =
+  match (filter_reachable operand) with | _, subtree -> set_union subtree tree
+and filter_reachable ((rt : 'N ), (tree : ('N * ('N, 'T) symbol list) list))  =
   if (List.assoc_opt rt tree) = None then  (rt, []) else
     let clipped_tree = (List.remove_assoc rt tree) in
-    let children : ('N, 'T) symbol list = List.assoc rt tree in
-    let nonterminal_children = (List.filter_map (function N node -> Some node | _ -> None) (children)) in
-    let filter_and_glue_subtrees_of = List.fold_right (map_and_merge_trees filter_reachable) in
-    let subgrammars = make_grammar nonterminal_children clipped_tree in
-    let subtree = filter_and_glue_subtrees_of (subgrammars) [(rt, children)] in
-    (rt, set_intersection tree (glue_subtrees_of [(rt, clipped_tree)] subtree))
+    let child : ('N, 'T) symbol list = List.assoc rt tree in
+    let child_N_symbols = (List.filter_map (function N node -> Some node | _ -> None) (child)) in
+    let filter_and_glue = List.fold_right filter_and_stick_subtree in
+    let subgrammars = make_grammar child_N_symbols clipped_tree in
+    let filtered_subtrees = filter_and_glue (subgrammars) [(rt, child)] in
+    (rt, set_intersection tree (filter_and_glue [(rt, clipped_tree)] filtered_subtrees))
