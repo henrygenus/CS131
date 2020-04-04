a685a9773d178a473143c7305a296420bf0fbe89
henrygenus <henry.a.genus@gmail.com>
2020-04-03 23:53:18 -0700

    Update hw1.ml

--- HW1/hw1.ml
+++ HW1/hw1.ml
@@ -28,19 +28,27 @@ let rec computed_fixed_point eq (f : 'a->'a) (x :'a) =
   let res = (f x) in
   match (eq res x) with
   | true -> x
-  | false-> (computed_fixed_point (eq) (f) res)
+  | false -> (computed_fixed_point (eq) (f) res)
 
 type ('nonterminal, 'terminal) symbol =
   | N of 'nonterminal
   | T of 'terminal
 
-let rec filter_reachable (g : 'N * ('N * ('N, 'T) symbol list) list)  =
-  let (rt, tree) = g in
-  let trimmed_tree = (List.remove_assoc rt tree) in
-  if (List.assoc_opt rt tree) = None then  ( "", []) else
-    let child = (List.assoc rt tree) in
-    let _, sub_tree = (filter_reachable (child, trimmed_tree)) in
-    let _, rem_tree = (filter_reachable (rt, trimmed_tree)) in
-    ((rt, set_intersection tree (set_union sub_tree ((rt, child)::rem_tree))))
+let rec make_pair_list (keys : 'N list) value =
+  match (keys) with
+  | [] -> []
+  | _ -> List.cons (List.hd keys, value) (make_pair_list (List.tl keys) value)
 
-(* fold union iter filter_reachable *)
+let merge_trees ((rt_one : 'N), tree_one) tree = set_union tree_one tree
+
+let filter_and_merge_trees f (g : 'N * ('N * ('N, 'T) symbol list) list) tree = merge_trees (f g) tree
+
+let rec filter_reachable (g : 'N * ('N * ('N, 'T) symbol list) list)  =
+  let ((rt : 'N) , (tree : ('N * ('N, 'T) symbol list) list)) = g in
+  if (List.assoc_opt rt tree) = None then  (rt, []) else
+    let (children : ('N, 'T) symbol list) = List.assoc rt tree in
+    let get_nonterminal = (function N node -> Some node | _ -> None) in
+    let nonterminal = (List.filter_map get_nonterminal (children)) in
+    let glue_subtrees = List.fold_right (filter_and_merge_trees filter_reachable) in
+    let subtree = glue_subtrees (make_pair_list nonterminal (List.remove_assoc rt tree)) [] in
+    (rt, set_intersection tree (List.cons (rt, children) subtree))
