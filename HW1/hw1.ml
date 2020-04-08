4887adfc687c10d7fe5f506863a9cc0d6ba61fea
henrygenus <henry.a.genus@gmail.com>
2020-04-07 21:03:20 -0700

    Update hw1.ml

--- HW1/hw1.ml
+++ HW1/hw1.ml
@@ -28,29 +28,28 @@ let rec computed_fixed_point eq f x =
   let res = (f x) in
   match (eq res x) with
   | true -> x
-  | false -> (computed_fixed_point (eq) (f) res)
+  | false -> (computed_fixed_point eq f res)
 
 type ('nonterminal, 'terminal) symbol =
   | N of 'nonterminal
   | T of 'terminal
 
-let rec symbol (a, b) = a
-and expression (a, b) = b
-and equal_sec_set a b = (equal_sets (List.nth a 1) (List.nth b 1))
-and get_reachable rules_symbols =
-  let rules, reachable_symbols = rules_symbols in
+let rec first (a, b) = a and second (a, b) = b
+and equal_sec_set rule_symbol_one rule_symbol_two =
+  let (_, a), (_, b) = (rule_symbol_one, rule_symbol_two) in (equal_sets a b)
+and get_reachable (rules, reachable_symbols) =
   if (rules = []) then (rules, reachable_symbols) else
     let rule = (List.hd rules) in
-    match (List.mem reachable_symbols (symbol rule)) with
+    match (List.mem (first rule) reachable_symbols) with
     | false -> get_reachable ((List.tl rules), reachable_symbols)
-    | true -> let nonterminal = List.filter (function N _ -> true | _ -> false) (expression rule) in
+    | true -> let nonterminal = List.filter_map (function N n -> Some n | T _ -> None) (second rule) in
               get_reachable ((List.tl rules), (set_union reachable_symbols nonterminal))
 and filter_reachable g =
   let root, rules = g in
   (* get reachable symbols *)
-  let _, reachable_symbols = computed_fixed_point equal_sec_set get_reachable (rules, (list root)) in
+  let _, reachable_symbols = computed_fixed_point equal_sec_set get_reachable (rules, [root]) in
   (* filter the rules *)
-  let filter_rules = (function s_r -> (List.mem (List.hd s_r) (List.nth s_r 1))) in
-  let filtered_rules = List.filter filter_rules (reachable_symbols, rules) in
+  let filter_rules = (function rule -> (List.mem (first rule) reachable_symbols)) in
+  let filtered_rules = List.filter filter_rules rules in
   (root, filtered_rules)
 
