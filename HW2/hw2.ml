2f3f4a34725b6e7eafc029b9b12bf5ea10892252
henrygenus <henry.a.genus@gmail.com>
2020-04-13 17:01:16 -0700

    Update hw2.ml

--- HW2/hw2.ml
+++ HW2/hw2.ml
@@ -27,5 +27,36 @@ let rec convert_rules gram1 =
 let convert_grammar gram1 =
   let grammar_two_rules = convert_rules gram1 in
   let start_symbol = (first gram1) in
-    (start_symbol, (fun rule -> List.assoc_opt rule grammar_two_rules))
+    (start_symbol, (fun rule -> List.assoc rule grammar_two_rules))
 
+let rec parse_tree_leaves tree =
+  match tree with
+    | Leaf l -> [l]
+    | Node (nonterminal, subtree) -> (List.concat (List.map parse_tree_leaves subtree))
+
+
+
+(* pruning -- need to increase efficiency *)
+let rec make_matcher gram =
+  let (root: 'N), (get_preds : 'N -> ('N, 'T) symbol list list)  = gram in
+  let preds = (get_preds root) in
+  (fun accept frag -> (apply_preds (get_preds) preds accept frag))
+and do_apply get_preds pred frag =
+    if (pred = []) then Some frag else
+    if (frag = []) then None else
+      let (sym : ('N, 'T) symbol) = (List.hd pred) in
+      match sym with
+      | N sym -> ((make_matcher (sym, get_preds)) (do_apply get_preds (List.tl pred)) frag)
+      | T sym -> if ((List.hd frag) = sym)
+                 then do_apply get_preds (List.tl pred) (List.tl frag)
+                 else None
+and apply_preds get_preds preds accept frag =
+  if (preds = []) then None else
+    match (do_apply get_preds (List.hd preds) frag) with
+    | None -> (apply_preds get_preds (List.tl preds) accept frag)
+    | Some suffix -> let return_value = (accept suffix) in
+                     if (return_value = None) then (apply_preds get_preds (List.tl preds) accept frag)
+                          else return_value
+
+(* parser calls matcher until end of fragment *)
+(*let make_parser *)
