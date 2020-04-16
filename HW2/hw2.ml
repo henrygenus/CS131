2da620c7093fc1984be6c27ef54ca55001834139
henrygenus <henry.a.genus@gmail.com>
2020-04-16 00:45:01 -0700

    Update hw2.ml

--- HW2/hw2.ml
+++ HW2/hw2.ml
@@ -8,21 +8,20 @@ type ('nonterminal, 'terminal) symbol =
 
 let first two_tuple = let ret, _ = two_tuple in ret
 let second two_tuple = let _, ret = two_tuple in ret
+let is_key key pair = (first pair) = key
 
 let rec convert_rules gram1 =
   let start_symbol, rules = gram1 in
   if ((List.length rules) = 0) then [] else
     (* get rules rooted at START_SYMBOL *)
-    let rules_for_symbol, rest = (List.partition (fun rule -> (first rule) = start_symbol) rules) in
+    let rules_for_symbol, rest = List.partition (is_key start_symbol) rules in
     (* merge the predicates of rules for start_symbol *)
     let _, alternative_list = List.split rules_for_symbol in
     (* if no rules left to convert, return grammar 2 type rules *)
     let g2_rules = [(start_symbol, alternative_list)] in
     if ((List.length rest) = 0) then g2_rules else
       (* else recursive call on rest of rules *)
-      let gram2 = (convert_rules ((first (List.hd rest)), rest)) in
-      (* return appended list of grammar2 style rules *)
-      g2_rules@gram2
+      g2_rules@(convert_rules ((first (List.hd rest)), rest))
 
 let convert_grammar gram1 =
   let grammar_two_rules = convert_rules gram1 in
@@ -32,25 +31,28 @@ let convert_grammar gram1 =
 let rec parse_tree_leaves tree =
   match tree with
     | Leaf l -> [l]
-    | Node (nonterminal, subtree) -> (List.concat (List.map parse_tree_leaves subtree))
+    | Node (_, subtree) -> (List.concat (List.map parse_tree_leaves subtree))
 
-let rec match_pred(prod_fcn : 'N -> ('N, 'T) symbol list list) pred frag =
+let rec complete_match prod_fcn pred frag =
+  let suf = match_pred prod_fcn pred frag in
+  match suf with
+  | Some [] -> Some []
+  | None -> None
+  | _ -> None
+and match_pred (prod_fcn : 'N -> ('N, 'T) symbol list list) pred frag =
   if (pred = []) then Some frag else if (frag = []) then None else
-    let sym = (List.hd pred) in
-    match sym with
-    | N n -> dfs prod_fcn (prod_fcn n) (match_pred prod_fcn (List.tl pred)) frag
-    | T (t : string) -> if (t = (List.hd frag))
-                        then match_pred prod_fcn (List.tl pred) (List.tl frag)
-                        else None
-and dfs prod_fcn associative_list accept frag =
-  if (associative_list = []) then None else
-    let pred_result = (match_pred prod_fcn (List.hd associative_list) frag) in
-    match pred_result with
-    | None -> dfs prod_fcn (List.tl associative_list) accept frag
-    | Some suffix -> let output = (accept suffix) in
-                     if (output = None)
-                     then dfs prod_fcn (List.tl associative_list) accept frag
-                     else output
+    match pred with
+    | (T (h : string))::t  -> if (h <> (List.hd frag)) then None
+                              else match_pred prod_fcn t (List.tl frag)
+    | (N h)::t -> let match_full_suffix = complete_match prod_fcn t in
+                  dfs prod_fcn (prod_fcn h) match_full_suffix frag
+and dfs prod_fcn assoc_list accept frag =
+  if (assoc_list = []) then None else
+    match (match_pred prod_fcn (List.hd assoc_list) frag) with
+    | None -> dfs prod_fcn (List.tl assoc_list) accept frag
+    | Some suffix -> match (accept suffix) with
+                     | Some output -> Some output
+                     | None ->  dfs prod_fcn (List.tl assoc_list) accept frag
 
 let rec make_matcher gram =
   let (root : 'n), (prod_fcn : 'N -> ('N, 'T) symbol list list) = gram in
@@ -60,4 +62,4 @@ let rec make_matcher gram =
 
 
 (* parser calls matcher until end of fragment *)
-(*let make_parser *)
+(* let make_parser *)
