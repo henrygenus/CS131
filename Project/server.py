8960262c1d405bb45179b4ec54fc8194d1b5ca77
henrygenus <henry.a.genus@gmail.com>
2020-05-24 03:01:44 -0700

    Update server.py

--- Project/server.py
+++ Project/server.py
@@ -1,5 +1,5 @@
 # import asyncio
-from time import clock_gettime, CLOCK_MONOTONIC
+from time import gmtime
 # import aiohttp
 
 # port 12000, 12001, 12002, 12003, 12004
@@ -18,31 +18,31 @@ class Server:
         message_handler = {
             "WHATSAT": (lambda msg: self.query_handler(msg)),
             "IMAT": (lambda msg: self.assertion_handler(msg)),
-            "AT": (lambda msg: self.message_handler(msg))
+            "AT": (lambda msg: self.report_handler(msg))
         }
-        response = message_handler[words[0]](words[1:])
+        response: Report = message_handler[words[0]](words[1:])
         self.log(response)
-
-    def query_handler(self, message):
-        client_name = message[0]
-        m_radius = message[1]
-        m_result_count = message[2]
-        pass
-
-    def assertion_handler(self, message):
-        client_name, long_lat, send_time = message
-        msg = Report("AT"
-                      + self.m_name
-                      + str(clock_gettime(CLOCK_MONOTONIC) - send_time)
-                      + client_name
-                      + long_lat
-                      + send_time)
-        self.log(msg)
-        # send msg to communicable servers
 
     def report_handler(self, message):
         self.log(Report(message))
         # send msg to communicable servers besides sender (message[1])
+
+    def assertion_handler(self, message):
+        client_name, long_lat, send_time = message
+        msg = Report("AT"
+                     + self.m_name
+                     + str(gmtime() - send_time)
+                     + client_name
+                     + long_lat
+                     + send_time)
+        self.log(msg)
+        # send msg to communicable servers
+
+    def query_handler(self, message):
+        """client_name = message[0]
+        m_radius = message[1]
+        m_result_count = message[2]"""
+        pass
 
     def log(self, msg):
         self.m_history[msg.m_client_name] = msg
@@ -78,7 +78,7 @@ class Report:
 
 
 def main():
-    srv = Server("Henry", 69)
+    Server("Henry", 69)
 
 
 if __name__ == "__main__":
