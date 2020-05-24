f485ff2d282ede4dca0c8babfb8944e59592fe51
henrygenus <henry.a.genus@gmail.com>
2020-05-24 02:46:32 -0700

    Update server.py

--- Project/server.py
+++ Project/server.py
@@ -1,25 +1,52 @@
-# import asynchio
+# import asyncio
+from time import clock_gettime, CLOCK_MONOTONIC
 # import aiohttp
-import json
-import time
-from message import *
 
 # port 12000, 12001, 12002, 12003, 12004
 # error checking not yet implemented
 
 
 class Server:
-    message_handler = {
-        "WHATSAT": (lambda message: QueryMessage(message)),
-        "IMAT": (lambda message: AssertionMessage(message)),
-        "AT": (lambda message: ReportMessage(message))
-    }
-    m_history = dict()
-
     def __init__(self, name, port):
         self.m_name = name
         self.m_port = port
         self.m_filename = str(name) + ".txt"
+        self.m_history = dict()
+
+    def process_message(self, message):
+        words = message.split()
+        message_handler = {
+            "WHATSAT": (lambda msg: self.query_handler(msg)),
+            "IMAT": (lambda msg: self.assertion_handler(msg)),
+            "AT": (lambda msg: self.message_handler(msg))
+        }
+        response = message_handler[words[0]](words[1:])
+        self.log(response)
+
+    def query_handler(self, message):
+        client_name = message[0]
+        m_radius = message[1]
+        m_result_count = message[2]
+        pass
+
+    def assertion_handler(self, message):
+        client_name, long_lat, send_time = message
+        msg = Report("AT"
+                      + self.m_name
+                      + str(clock_gettime(CLOCK_MONOTONIC) - send_time)
+                      + client_name
+                      + long_lat
+                      + send_time)
+        self.log(msg)
+        # send msg to communicable servers
+
+    def report_handler(self, message):
+        self.log(Report(message))
+        # send msg to communicable servers besides sender (message[1])
+
+    def log(self, msg):
+        self.m_history[msg.m_client_name] = msg
+        # write msg() to log file
 
     def run(self):
         # loop with asynch I/O
@@ -29,15 +56,25 @@ class Server:
         # m_history[msg.clientName()] = msg
         pass
 
-    @staticmethod
-    def process_message(message):
-        Server.message_handler[message[0]](message[1:])
 
-# message interface:
-#    __init__(message)
-#    __call__()
-#    get_client_name()
-# anything else can be below the surface
+class Report:
+    def __init__(self, message):
+        self.m_server_name = message[1]
+        self.m_time_dif = message[2]
+        self.m_client_name = message[3]
+        self.m_long_lat = message[4]
+        self.m_send_time = message[5]
+
+    def __call__(self):
+        return "AT" \
+               + self.m_server_name \
+               + self.m_time_dif \
+               + self.m_client_name \
+               + self.m_long_lat \
+               + self.m_send_time
+
+    def get_client_name(self):
+        return self.m_client_name
 
 
 def main():
