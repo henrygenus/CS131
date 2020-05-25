87fbf2e649846bdd698dc287f433d04c3235727e
henrygenus <henry.a.genus@gmail.com>
2020-05-24 20:49:22 -0700

    first test

--- Project/server.py
+++ Project/server.py
@@ -1,5 +1,5 @@
 # import asyncio
-from time import gmtime
+from time import time
 # import aiohttp
 
 # port 12000, 12001, 12002, 12003, 12004
@@ -10,67 +10,77 @@ class Server:
     def __init__(self, name, port):
         self.m_name = name
         self.m_port = port
-        self.m_filename = str(name) + ".txt"
+        self.m_filename = str(name) + "_log.txt"
         self.m_history = dict()
+        self.m_connections = dict()
 
     def process_message(self, message):
         words = message.split()
         message_handler = {
             "WHATSAT": (lambda msg: self.query_handler(msg)),
-            "IMAT": (lambda msg: self.assertion_handler(msg)),
+            "IAMAT": (lambda msg: self.assertion_handler(msg)),
             "AT": (lambda msg: self.report_handler(msg))
         }
         response: Report = message_handler[words[0]](words[1:])
-        self.log(response)
+        return self.log(response)
 
     def report_handler(self, message):
-        self.log(Report(message))
         # send msg to communicable servers besides sender (message[1])
+        return Report(message)
+
+    def connect_server(self, servers):
+        for name, port_number in servers:
+            self.m_connections[name] = port_number
 
     def assertion_handler(self, message):
         client_name, long_lat, send_time = message
-        msg = Report("AT"
-                     + self.m_name
-                     + str(gmtime() - send_time)
-                     + client_name
-                     + long_lat
-                     + send_time)
-        self.log(msg)
+        msg = Report([self.m_name,
+                      str(time() - float(send_time)),
+                      client_name,
+                      long_lat,
+                      send_time])
         # send msg to communicable servers
+        return msg
 
     def query_handler(self, message):
-        """client_name = message[0]
-        m_radius = message[1]
-        m_result_count = message[2]"""
-        pass
+        client_name, m_radius, m_result_count = message
+        msg = self.m_history[client_name]
+        # ask google for location
+        # send message and JSON back to client
+        return msg
 
     def log(self, msg):
         self.m_history[msg.m_client_name] = msg
-        # write msg() to log file
+        with open(self.m_filename, 'w+') as logfile:
+            logfile.write(msg())
 
     def run(self):
-        # loop with asynch I/O
-        # asynch read (message)
-        # log message
+        # loop with async I/O
+        # (async) await read (message)
         # msg = processMessage(message)
-        # m_history[msg.clientName()] = msg
         pass
+
+    def dump(self):
+        for message in self.m_history.values():
+            print(message())
+        for server in self.m_connections.keys():
+            print("Connected to:" + server)
 
 
 class Report:
     def __init__(self, message):
-        self.m_server_name = message[1]
-        self.m_time_dif = message[2]
-        self.m_client_name = message[3]
-        self.m_long_lat = message[4]
-        self.m_send_time = message[5]
+        self.m_server_name,\
+            self.m_time_dif, \
+            self.m_client_name, \
+            self.m_long_lat, \
+            self.m_send_time = message
 
     def __call__(self):
-        return "AT" \
-               + self.m_server_name \
-               + self.m_time_dif \
-               + self.m_client_name \
-               + self.m_long_lat \
+        return "AT " \
+               + self.m_server_name + " "\
+               + self.m_time_dif + " "\
+               + self.m_client_name + " "\
+               + self.m_long_lat + " "\
                + self.m_send_time
 
     def get_client_name(self):
@@ -78,7 +88,18 @@ class Report:
 
 
 def main():
-    Server("Henry", 69)
+    srv = Server("test", 69)
+    srv.process_message("IAMAT "
+                        "kiwi.cs.ucla.edu "
+                        "0 "
+                        "0")
+    srv.process_message("AT Hill "
+                        "+0.263873386 "
+                        "kiwi.cs.ucla.edu "
+                        "+34.068930-118.445127 "
+                        "1520023934.918963997")
+    srv.process_message("WHATSAT kiwi.cs.ucla.edu 10 5")
+    srv.dump()
 
 
 if __name__ == "__main__":
